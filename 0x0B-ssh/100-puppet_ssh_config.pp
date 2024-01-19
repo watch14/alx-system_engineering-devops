a71de0618cc9fd10d79f50e9ba20c2e2c8659a13
watch14 <watchman1414@gmail.com>
2024-01-19 23:14:52 +0000

    fast_push

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# conf
+
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path   => '/home/your_username/.ssh/config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?IdentityFile',
+}
