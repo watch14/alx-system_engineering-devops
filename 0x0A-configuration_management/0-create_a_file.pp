3e27e0f5c2d6dd33d7f612c55156c1b2d82631ed
watch14 <watchman1414@gmail.com>
2024-01-19 22:39:38 +0000

    fast_push

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# File Create
-
+# Creates a file
 file { '/tmp/school':
-  path    => '/tmp/school'
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
