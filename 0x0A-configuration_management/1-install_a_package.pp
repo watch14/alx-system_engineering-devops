56098fc2c94d6f3de3cfe944626d5e29d1ea3967
watch14 <watchman1414@gmail.com>
2024-01-19 22:58:12 +0000

    fast_push

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-#!/usr/bin/pup
-# Install Flask version 2.1.0 using pip3
-package { 'flask':
+# 1-install_a_package.pp
+
+# X
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+# pack
+package { 'python3-pip':
+  ensure => 'installed',
 }
