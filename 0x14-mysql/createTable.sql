263553499bc5583a6b47344e0a768d4e7ba5c173
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-13 08:37:03 +0000

    fast_push

--- 0x14-mysql/createTable.sql
+++ 0x14-mysql/createTable.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE tyrell_corp;
 USE tyrell_corp;
 CREATE TABLE nexus6(id INTEGER, name TEXT);
-INSERT INTO nexus6 VALUES (0, "Jarvis");
+INSERT INTO nexus6 VALUES (0, "watch");
 GRANT SELECT ON tyrell_corp.nexus6 TO holberton_user@localhost;
