f302e520254ca0c65d56bceb6d9e73efdd492ee8
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-19 08:24:40 +0000

    fast_push

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" API TODOS """
+import requests
+import json
+from sys import argv
+
+user_id = argv[1]
+
+todo_link = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+user_link = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+
+user_resp = requests.get(user_link)
+todo_resp = requests.get(todo_link)
+
+user_data = user_resp.json()
+todo_data = todo_resp.json()
+
+print(user_data["name"])
+
+tasks = 0
+done = 0
+completed = []
+for todo_item in todo_data:
+    tasks += 1
+
+    if todo_item["completed"]:
+            completed.append(todo_item["title"])
+            done += 1
+
+print (f"Employee {user_data['name']} is done with tasks ({done}/{tasks}):")
+for i in completed:
+    print (f"\t {i}")
