41cd5166dde62281fb725496a46b0e734fee3d64
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-02-19 09:08:36 +0000

    fast_push

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,27 +3,29 @@
 import requests
 from sys import argv
 
-user_id = argv[1]
 
-todo_link = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-user_link = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+if __name__ == "__main__":
+    user_id = argv[1]
 
-user_resp = requests.get(user_link)
-todo_resp = requests.get(todo_link)
+    todo_link = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    user_link = f"https://jsonplaceholder.typicode.com/users/{user_id}"
 
-user_data = user_resp.json()
-todo_data = todo_resp.json()
+    user_resp = requests.get(user_link)
+    todo_resp = requests.get(todo_link)
 
-tasks = 0
-done = 0
-completed = []
-for todo_item in todo_data:
-    tasks += 1
+    user_data = user_resp.json()
+    todo_data = todo_resp.json()
 
-    if todo_item["completed"]:
-        completed.append(todo_item["title"])
-        done += 1
+    tasks = 0
+    done = 0
+    completed = []
+    for todo_item in todo_data:
+        tasks += 1
 
-print (f"Employee {user_data['name']} is done with tasks ({done}/{tasks}):")
-for i in completed:
-    print (f"\t {i}")
+        if todo_item["completed"]:
+            completed.append(todo_item["title"])
+            done += 1
+
+    print(f"Employee {user_data['name']} is done with tasks ({done}/{tasks}):")
+    for i in completed:
+        print (f"\t {i}")
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,23 +4,25 @@ import requests
 import csv
 from sys import argv
 
-user_id = argv[1]
+if __name__ == "__main__":
+    user_id = argv[1]
 
-todo_link = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-user_link = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todo_link = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    user_link = f"https://jsonplaceholder.typicode.com/users/{user_id}"
 
-user_resp = requests.get(user_link)
-todo_resp = requests.get(todo_link)
+    user_resp = requests.get(user_link)
+    todo_resp = requests.get(todo_link)
 
-user_data = user_resp.json()
-todo_data = todo_resp.json()
+    user_data = user_resp.json()
+    todo_data = todo_resp.json()
 
-user = user_data["username"]
+    user = user_data["username"]
 
-file_name = f"{user_id}.csv"
-with open(file_name, mode="w", newline='') as csv_file:
-    writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    file_name = f"{user_id}.csv"
+    with open(file_name, mode="w", newline='') as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
-    for todo_item in todo_data:
-        writer.writerow(
-                [user_id, user, todo_item['completed'], todo_item['title']])
+        for todo_item in todo_data:
+            writer.writerow(
+                    [user_id, user, todo_item['completed'], todo_item['title']]
+                    )
