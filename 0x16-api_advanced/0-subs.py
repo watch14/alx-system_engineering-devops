e5bc18d02dda55ddeb8607d7a800c24b9eed9240
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-03-05 19:03:24 +0100

    fast_push

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-""" API Subreddit """
+"""
+API Subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ nbr subs """
+    """
+    nbr subs
+    """
     link = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'ALX aAPI task 0'}
     resp = requests.get(link, headers=headers, allow_redirects=False)
