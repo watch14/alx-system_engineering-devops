660f99cb20c22726f5b32ae700261b9aa6ba9c4a
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-03-05 20:27:14 +0100

    fast_push

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# Fixes site on apache
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+# Fixe name
+exec { 'rename file':
+  command => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
 }
