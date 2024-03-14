8521df2ac9db57d54323e4c6de8c64ae1628298b
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-03-14 03:50:39 +0100

    fast_push

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# fix for limit 
+exec { 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 5000\"/g" /etc/default/nginx':
+  path => '/usr/bin:/usr/sbin:/bin',
+}
+-> exec {'restart service':
+  command => '/usr/sbin/service nginx restart',
+}
